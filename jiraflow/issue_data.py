4476d28c0e7d45f70bf4224929cff070cae49996
Razzi Abuissa <razzi53@gmail.com>
2019-10-28 11:18:54 -0700

    Scripts updates

--- jiraflow/issue_data.py
+++ jiraflow/issue_data.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python3.7
-
 import os
 import sys
 
 import requests
 
-from lib import JIRA_SEARCH_URL, OPEN_ISSUES_JQL
+from jiraflow.lib import JIRA_SEARCH_URL, OPEN_ISSUES_JQL
 
 
-response = requests.get(
-    JIRA_SEARCH_URL,
-    params={
-        'jql': OPEN_ISSUES_JQL,
-        'maxResults': 1,
-    },
-    headers={'Authorization': os.environ['JIRA_AUTH']},
-)
+def main():
+    response = requests.get(
+        JIRA_SEARCH_URL,
+        params={
+            'jql': OPEN_ISSUES_JQL,
+            'maxResults': 1,
+        },
+        headers={'Authorization': os.environ['JIRA_AUTH']},
+    )
 
-sys.stdout.buffer.write(response.content)
+    sys.stdout.buffer.write(response.content)
